050b8bcf15ea1bef2e309f2dd93e833d1cc84d28
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-01-22 16:27:15 +0100

    Premières correction DataBase et Data

--- CreationBD_SportsDeCombat .sql
+++ CreationBD_SportsDeCombat .sql
@@ -9,13 +9,39 @@ END
 --adapter le chemin aux fichiers de données et de logs
 CREATE DATABASE SportsDeCombat
 	ON  PRIMARY 
-	( NAME = 'SportsDeCombat', FILENAME = 'C:\Data\Cours\2019-2020\MA-08\semaine 3\SportsDeCombat.mdf' , SIZE = 20480KB , MAXSIZE = 51200KB, FILEGROWTH = 1024KB )
+	
+	( NAME = 'SportsDeCombat', FILENAME = 'C:\DATA\MSSQL\SportsDeCombat.mdf' , SIZE = 20480KB , MAXSIZE = 51200KB , FILEGROWTH = 1024KB )
 	LOG ON 
-	( NAME = 'SportsDeCombat_log', FILENAME = 'C:\Data\Cours\2019-2020\MA-08\semaine 3\SportsDeCombat_log.ldf' , SIZE = 10240KB , MAXSIZE = 20480KB , FILEGROWTH = 1024KB)
+	( NAME = 'SportsDeCombat_log', FILENAME = 'C:\DATA\MSSQL\SportsDeCombat.ldf' , SIZE = 10240KB , MAXSIZE = 20480KB , FILEGROWTH = 1024KB )
+
 go
 
 USE SportsDeCombat
 go
+
+create table Styles (
+	id int NOT NULL PRIMARY KEY,
+	Name varchar(45) NOT NULL,
+	Level_Style varchar(45)
+
+)
+
+create table Gears (
+	id int NOT NULL PRIMARY KEY,
+	Type_Gear varchar(45),
+	Brand varchar(30),
+	Serial_Number varchar(30)
+)
+
+create table "Statistics" (
+    id int NOT NULL PRIMARY KEY,
+	KO int,
+	TKO int ,
+	Declassification int, 
+	Split_Decision int, 
+	Submission int, 
+	Rank_Fighter int NOT NULL
+)
 
 create table Fighters (
 	id int NOT NULL PRIMARY KEY,
@@ -23,18 +49,22 @@ create table Fighters (
 	FirstName varchar(30)NOT NULL,
 	Alias varchar(30),
 	Weight int NOT NULL,
-	heigh int NOT NULL	,
+	heigh int NOT NULL,
 	reach int NOT NULL,
-	PersonalInformations_id int FOREIGN KEY REFERENCES PersonalInformations(id),
 	Styles_id int FOREIGN KEY REFERENCES Styles(id),
-	Clubs_id int FOREIGN KEY REFERENCES PersonalInformations(id),
 	Gears_id int FOREIGN KEY REFERENCES Gears(id),
-	Status_id int FOREIGN KEY REFERENCES Status(id),
 	Age int NOT NULL,
 	Address varchar(45)NOT NULL,
-	Tel_Number int ,
-	Salary int
+	Tel_Number int,
+	Salary int,
+	Licence_Number varchar(45)
 	
+)
+
+create table Status (
+    id int NOT NULL PRIMARY KEY,
+	Name varchar(30) NOT NULL ,
+	Fighters_id int FOREIGN KEY REFERENCES Fighters(id),
 )
 
 create table Sponsors (
@@ -56,7 +86,7 @@ create table Contests (
 )
 
 create table Contests_has_Fighters (
-	Fighters_id int FOREIGN KEY REFERENCES Fights(id),
+	Fighters_id int FOREIGN KEY REFERENCES Fighters(id),
 	Contests_id int FOREIGN KEY REFERENCES Contests(id)
 )
 
@@ -67,7 +97,14 @@ create table Categories (
 
 create table Fighters_has_Categories (
    Fighters_id int FOREIGN KEY REFERENCES Fighters(id),
-   Categories_id int FOREIGN KEY REFERENCES Categories(id))
+   Categories_id int FOREIGN KEY REFERENCES Categories(id)
+)
+
+create table Titles (
+	id int NOT NULL PRIMARY KEY,
+	Name varchar(30) NOT NULL UNIQUE,
+	Nb_times int ,
+	Date_title date
 )
 
 create table Fights (
@@ -77,58 +114,25 @@ create table Fights (
 	town varchar(45),
 	address varchar(45),
 	date_fight date,
-	category varchar(45)
+	category varchar(45),
 	loser varchar(45),
 	winner varchar(45),
 	Sport_type varchar(45),
 	Result_fight varchar(45),
-	Title_idTitle int FOREIGN KEY REFERENCES Title(idTitle)
+	Titles_id int FOREIGN KEY REFERENCES Titles(id)
 	
 )
 
 create table Fighters_has_Fights (
 	Fighters_id int FOREIGN KEY REFERENCES Fighters(id),
-    Fight_id int FOREIGN KEY REFERENCES Fight(id)
+    Fight_id int FOREIGN KEY REFERENCES Fights(id)
 )
 
-create table Statistics (
-    id int NOT NULL PRIMARY KEY,
-	KO int,
-	TKO int ,
-	Declassification int, 
-	Split_Decision int, 
-	Submission int, 
-	Rank_Fighter int NOT NULL
-)
 
 create table Fighters_has_Statistics (
-    Fight_id int FOREIGN KEY REFERENCES Fight(id),
-    Statistics_id int FOREIGN KEY REFERENCES Statistics(id)
+    Fighters_id int FOREIGN KEY REFERENCES Fighters(id),
+    Statistics_id int FOREIGN KEY REFERENCES "Statistics"(id)
 )
-create table Titles (
-	id int NOT NULL PRIMARY KEY,
-	Name varchar(30) NOT NULL UNIQUE,
-	Nb_times int ,
-	Date_title date
-)
-
-create table Styles (
-	id int NOT NULL PRIMARY KEY,
-	Name varchar(45) NOT NULL,
-	Level_Style varchar(45)
-
-
-
-)
-
-create table Gears (
-	id int NOT NULL PRIMARY KEY,
-	Type_Gear varchar(45),
-	Brand varchar(30),
-	Serial_Number varchar(30)
-)
-
-
 
 create table Clubs (
 	 id int NOT NULL PRIMARY KEY,
