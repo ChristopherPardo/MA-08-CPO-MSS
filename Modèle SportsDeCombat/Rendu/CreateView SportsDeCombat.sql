493f53f31f446d801218fdb196e34d3d85f19616
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-01-26 19:13:29 +0100

    Final commit

--- Modèle SportsDeCombat/Rendu/CreateView SportsDeCombat.sql
+++ Modèle SportsDeCombat/Rendu/CreateView SportsDeCombat.sql
@@ -1,6 +1,12 @@
+/*
+ * Authors : Christopher Miguel
+ * Project : Sports de combat
+ * Date : 26.01.2020
+*/
+
 SELECT T.name AS table_name,
   PS.row_count
 FROM sys.dm_db_partition_stats AS PS
 JOIN sys.tables AS T ON PS.object_id = T.object_id
 WHERE PS.index_id BETWEEN 0 AND 1
-ORDER BY T.name
+ORDER BY T.name;
